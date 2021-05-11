fbf9471c9e52a0ebd1a6ffb88851300deb5f7b96
Arui <41237117+AruiGit@users.noreply.github.com>
2021-05-11 17:30:02 +0200

    Enemy update
    Player finding and moving towards him

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,12 +10,16 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float lookingAngle = 0;
     [SerializeField] private float fov = 90;
     [SerializeField] private float viewDistance = 2f;
+    [SerializeField] private float speed = 0.05f;
+
+    private GameObject player;
+    private Vector3 lastKnowPlayerPosition;
 
     private void Start()
     {
         fieldofview = Instantiate(prefabFieldOfView, null).GetComponent<FieldOfView>();
         fieldofview.SetOrigin(transform.position);
-        
+        player = GameObject.Find("Player_Character");
        
     }
 
@@ -24,12 +28,23 @@ public class Enemy : MonoBehaviour
         fieldofview.SetAimDirection(lookingAngle);
         fieldofview.SetFoV(fov);
         fieldofview.SetViewDistance(viewDistance);
+        fieldofview.SetOrigin(transform.position);
+
+        FindPlayer();
     }
 
 
     private void AttackPlayer()
     {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
 
-
+    private void FindPlayer()
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < viewDistance)
+        {
+            lastKnowPlayerPosition = player.transform.position;
+            AttackPlayer();
+        } 
     }
 }
