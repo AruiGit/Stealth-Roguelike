1bd63e980afb52012746b095c538d73d6323e2e8
Arui <41237117+AruiGit@users.noreply.github.com>
2021-06-08 16:40:34 +0200

    Enemy
    Added turret with script that follows Player movement

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    bool enemyVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        Debug.Log("Collision!");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            Debug.Log("Collision with PLAYER!");
+            RaycastHit2D environmentCheck = Physics2D.Raycast(this.transform.position, (collision.transform.position - transform.position).normalized, 1200, ~LayerMask.GetMask("Enemy"));
+            Debug.Log(environmentCheck.collider);
+            if (environmentCheck.collider.CompareTag("Player"))
+            {
+                enemyVisible = true;
+                transform.up = collision.gameObject.transform.position - transform.position;
+            }
+            else
+            {
+                enemyVisible = false;
+            }
+        }
+    }
+   
+}
