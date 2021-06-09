6dd7741c2eaea734cc26f25726908daaa500e5d6
Arui <41237117+AruiGit@users.noreply.github.com>
2021-06-09 17:25:59 +0200

    Sprites
    Deleting unused sprites

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-
+    public Sprite turretON, turretOFF, turretIDLE;
     bool enemyVisible;
+    bool isTurretON = true;
+    bool canShoot = true;
+    float bulletSpeed = 1f;
+    float attackSpeed = 1f;
+    public Transform bulletSpawn, bulletSpawn1;
+
+
+    public GameObject bulletPrefab;
+    bool cannonSwitch = true;
 
     // Start is called before the first frame update
     void Start()
@@ -14,31 +23,67 @@ public class Turret : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (enemyVisible == false && isTurretON == true)
+        {
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite = turretIDLE;
+        }
+        if(enemyVisible==true && isTurretON==true && canShoot == true)
+        {
+            AttackPlayer();
+            Debug.Log("Attacking Player");
+            Debug.Log("enemyVisible: " + enemyVisible + " isTurretON: " + isTurretON + " canShoot: " + canShoot);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
 
-        Debug.Log("Collision!");
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player")&& isTurretON==true)
         {
 
-            Debug.Log("Collision with PLAYER!");
             RaycastHit2D environmentCheck = Physics2D.Raycast(this.transform.position, (collision.transform.position - transform.position).normalized, 1200, ~LayerMask.GetMask("Enemy"));
             Debug.Log(environmentCheck.collider);
             if (environmentCheck.collider.CompareTag("Player"))
             {
                 enemyVisible = true;
                 transform.up = collision.gameObject.transform.position - transform.position;
+                Sprite sprite = GetComponent<SpriteRenderer>().sprite = turretON;
             }
             else
             {
                 enemyVisible = false;
+                
             }
         }
     }
-   
+
+    void AttackPlayer()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    private IEnumerator Shoot()
+    {
+        Debug.Log("In Shoot IEnumerator");
+        canShoot = false;
+        if (cannonSwitch == true)
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            newBullet.GetComponent<Rigidbody2D>().AddForce(newBullet.transform.right * bulletSpeed, ForceMode2D.Impulse);
+            cannonSwitch = false;
+        }
+        else
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, bulletSpawn1.position, bulletSpawn1.rotation);
+            newBullet.GetComponent<Rigidbody2D>().AddForce(newBullet.transform.right * bulletSpeed, ForceMode2D.Impulse);
+            cannonSwitch = true;
+        }
+         // We use the right (or up in some cases) transform because forward in a 2D space is into the screen.
+        yield return new WaitForSeconds(attackSpeed);
+        Debug.Log("after waitforsecnd");
+        canShoot = true;
+    }
+
 }
