4045a7ff061271780fa305e3776d897dfde1de63
Arui <41237117+AruiGit@users.noreply.github.com>
2021-06-10 14:30:25 +0200

    Revert "Revert "Bullet, Player""
    This reverts commit f9f66ca045f9f2009bf9746b9341fb2a517f1d08.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Player player;
+    public GameObject sprite, capsule;
+    int damage;
+
     void Start()
+    {
+        player = GameObject.Find("Player_Character").GetComponent<Player>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+            DestroyAll();
+        }
+        else if (!collision.gameObject.CompareTag("Enemy"))
+        {
+            DestroyAll();
+        }
         
+    }
+
+    void DestroyAll()
+    {
+        Destroy(capsule);
+        Destroy(sprite);
+        Destroy(gameObject);
+    }
+
+    public void SetDamage(int damage)
+    {
+        this.damage = damage;
     }
 }
